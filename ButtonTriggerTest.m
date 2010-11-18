7df6f9e8bdaa79dc7291889f6ce3a195a96240b4
Jason Moore <moorepants@gmail.com>
2010-11-17 23:02:24 -0800

    added the vnav data collection to the trigger callback but the vnav is starting before the DAQ, must be something to do with the serial buffer starting before the DAQ

--- ButtonTriggerTest.m
+++ ButtonTriggerTest.m
@@ -1,4 +1,4 @@
-function ButtonTriggerTest
+ function [nidata, time, abstime, events] = ButtonTriggerTest
 % testing external triggering and trigger callback functions
 
 % you have to delete the ai object for things to restart correctly
@@ -24,15 +24,18 @@ chan = addchannel(ai, [0 17]); % pot is in AI0 and button is in AI17
 set(ai,'InputType','SingleEnded')
 set(ai,'SampleRate',samplerate)
 set(ai,'SamplesPerTrigger',duration*get(ai,'SampleRate'))
+
+% configure the trigger
 set(ai,'TriggerType','Software')
 set(ai,'TriggerChannel',chan(2))
 set(ai,'TriggerConditionValue',4.9) % trigger if button goes above 4.9 volts
 set(ai,'TriggerFcn',@TriggerCallback)
+
 % start up the DAQ
 start(ai)
-wait(ai, 5*duration)
+display('DAQ has started')
+wait(ai, 5*duration) % wait so that getdata doesn't try to get too early
 [nidata, time, abstime, events] = getdata(ai);
-
 stop(ai)
 
 function TriggerCallback(obj, event)
--- NI_VN_exp.m
+++ NI_VN_exp.m
@@ -1,5 +1,5 @@
-function NI_VN_exp
-% gilbert's test code to see if the NI Daq card and the vectornav play well
+function [nidata, time, abstime, events, ai] = NI_VN_exp
+% test code to see if the NI Daq card and the vectornav play well
 % together
 
 % you have to delete the ai object for things to restart correctly
@@ -11,80 +11,77 @@ clc
 close all;
 clear all;
 
-% load the VectorNav library
-addpath('C:\Documents and Settings\Administrator\My Documents\MATLAB\VectorNavLib')
-
+% daq parameters
 samplerate = 100; % sample rate in hz
 duration = 5; % the sample time in seconds
 numsamples = duration*samplerate;
 
+% connect to the NI USB-6218
+ai=analoginput('nidaq', 'Dev1');
+
+% add channels and lines
+chan = addchannel(ai, [0 17]); % pot is in AI0 and button is in AI17
+
+% configure the DAQ
+set(ai, 'InputType', 'SingleEnded')
+set(ai,'SampleRate',samplerate)
+set(ai,'SamplesPerTrigger',duration*get(ai,'SampleRate'))
+
+% trigger details
+set(ai,'TriggerType','Software')
+set(ai,'TriggerChannel',chan(2))
+set(ai,'TriggerConditionValue',4.9) % trigger if button goes above 4.9 v
+set(ai,'TriggerDelay',0.09)
+
+% load the VectorNav library
+addpath('C:\Documents and Settings\Administrator\My Documents\MATLAB\VectorNavLib')
 % connect to the VectorNav
 s = VNserial('COM3');
 % set the data output rate
 VNwriteregister(s, 7, samplerate);
 % set the output type: 'YPR'
-%VNwriteregister(s, 6, 1);
+VNwriteregister(s, 6, 1);
 % initialize the data
-%vndata = zeros(samplerate*duration, 3);
-VNwriteregister(s, 6, 14);
-vndata = zeros(samplerate*duration, 12);
-vntextdata = cell(samplerate*duration, 1);
+vndata = zeros(samplerate*duration, 3);
 %Create parse string
 ps = '%*6c';
 for i=1:size(vndata, 2)
     ps = [ps ',%g'];
 end
 
-% connect to the NI USB-6218
-ai=analoginput('nidaq','Dev1');
-
-% configure the DAQ
-set(ai,'InputType','SingleEnded')
-set(ai,'SampleRate',samplerate)
-set(ai,'SamplesPerTrigger',duration*get(ai,'SampleRate'))
-set(ai,'TriggerType','Software')
-set(ai,'TriggerDelay',0.0)
-set(ai,'TriggerFcn',{@TriggerCallback,duration,samplerate,numsamples,vndata})
-chan = addchannel(ai,[0 17]);
-
-
-vntime = zeros(numsamples,1);
+set(ai,'TriggerFcn',{@TriggerCallback,s,ps,duration,samplerate,numsamples,vndata})
 
 % start up the DAQ
 start(ai)
 display('DAQ started')
-trig = 0;
-
-
-toc
-display('VN data done')
-%Turn off ADOR
-VNprintf(s, 'VNWRG,6,0');
-pause(0.1);
-VNclearbuffer(s);
+wait(ai, 5*duration)
 
 % vndata = VNrecordADOR(s, 'YPR', samplerate, duration);
 
-daqdata = getdata(ai);
+[nidata, time, abstime, events] = getdata(ai);
 %daqdata = peekdata(ai, numsamples)
-
+vndata = ai.UserData;
 stop(ai)
-delete(ai)
+% delete(ai)
 
 % plot versus sample
 figure(1)
-plot(1:numsamples,-(vndata(:,1)-vndata(1,1)),1:numsamples,21*(daqdata-daqdata(1)))
-figure(2)
-% plot versus time
-plot(vntime(:),-(vndata(:,1)-vndata(1,1)),0:1/samplerate:duration-1/samplerate,21*(daqdata-daqdata(1)))
+plot(1:numsamples,-(vndata(:,1)-vndata(1,1)),1:numsamples,21*(nidata-nidata(1)))
 legend('vndata', 'daqdata')
-figure(3)
-plot(vntime)
 
-function TriggerCallback(obj, event, duration, samplerate, numsamples, data)
+function TriggerCallback(obj, event, s, ps, duration, samplerate, numsamples, vndata)
+display('Trigger called')
 % record data
-for i=1:duration
-    for j=1:samplerate
-        data((i-1)*samplerate+j, :) = fscanf(s, ps);
-    end
-end
+vndata = VNrecordADOR(s, 'YPR', samplerate, duration);
+% for i=1:duration
+%     for j=1:samplerate
+%         vndata((i-1)*samplerate+j, :) = fscanf(s, ps);
+%     end
+%     display('a sec')
+% end
+obj.UserData = vndata;
+% display('VN data done')
+% %Turn off ADOR
+% VNprintf(s, 'VNWRG,6,0');
+% pause(0.1);
+% VNclearbuffer(s);
