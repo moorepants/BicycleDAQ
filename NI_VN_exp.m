603ae2f61069605078ed68b7d8dc2c17db6d7f4a
Jason Moore <moorepants@gmail.com>
2010-11-19 09:50:41 -0800

    this code just made the signals line up exactly, holy shit

--- NI_VN_exp.m
+++ NI_VN_exp.m
@@ -43,13 +43,14 @@ VNwriteregister(s, 7, samplerate);
 VNwriteregister(s, 6, 1);
 % initialize the data
 vndata = zeros(samplerate*duration, 3);
+vndatatext = cell(samplerate*duration, 1);
 %Create parse string
 ps = '%*6c';
 for i=1:size(vndata, 2)
     ps = [ps ',%g'];
 end
 
-set(ai,'TriggerFcn',{@TriggerCallback,s,ps,duration,samplerate,numsamples,vndata})
+set(ai,'TriggerFcn',{@TriggerCallback,s,ps,duration,samplerate,numsamples,vndata,vndatatext})
 
 % start up the DAQ
 start(ai)
@@ -58,27 +59,37 @@ wait(ai, 5*duration)
 
 [nidata, time, abstime, events] = getdata(ai);
 %daqdata = peekdata(ai, numsamples)
-vndata = ai.UserData;
+vndatatext = ai.UserData;
 stop(ai)
 % delete(ai)
+
+% process the text data
+for i=1:numsamples
+    try
+        vndata(i, :) = sscanf(vndatatext{i}, ps);
+    catch
+        vndata(i) = NaN;
+    end
+end
 
 % plot versus sample
 figure(1)
 plot(1:numsamples,-(vndata(:,1)-vndata(1,1)),1:numsamples,21*(nidata-nidata(1)))
 legend('vndata', 'daqdata')
 
-function TriggerCallback(obj, event, s, ps, duration, samplerate, numsamples, vndata)
+function TriggerCallback(obj, event, s, ps, duration, samplerate, numsamples, vndata, vndatatext)
 display('Trigger called')
 % record data
 % vndata = VNrecordADOR(s, 'YPR', samplerate, duration);
-pause(0.1)
+
 for i=1:duration
     for j=1:samplerate
-        vndata((i-1)*samplerate+j, :) = fscanf(s, ps);
+        vndatatext{(i-1)*samplerate+j} = fgets(s);
+%         vndata((i-1)*samplerate+j, :) = fscanf(s, ps);
     end
     display('a sec')
 end
-obj.UserData = vndata;
+obj.UserData = vndatatext;
 display('VN data done')
 %Turn off ADOR
 VNprintf(s, 'VNWRG,6,0');
