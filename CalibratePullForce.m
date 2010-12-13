b4faac4d844ec59dc9960442dddf07e80003f6bd
Jason Moore <moorepants@gmail.com>
2010-12-13 12:29:41 -0800

    little script to calibrate the pull force sensor

---
+++ CalibratePullForce.m
@@ -0,0 +1,43 @@
+% you have to delete the ai object for things to restart correctly
+if exist('ai')
+    delete(ai)
+end
+% clear up everything else
+close all; clear all; clc;
+
+% connect to the daq box
+ai = analoginput('nidaq','Dev1');
+duration = 5; % seconds
+set(ai,'SampleRate',200)
+ActualRate = get(ai,'SampleRate');
+set(ai,'SamplesPerTrigger',duration*ActualRate);
+set(ai,'InputType','SingleEnded');
+chan = addchannel(ai,[21]);
+
+% how many trials will you do?
+numTrials = str2num(input('How many trials?\n', 's'));
+p = zeros(numTrials, 1);
+v = zeros(numTrials, 1);
+
+for i = 1:length(p)
+    % ask the user to enter the number of pounds added
+    weightadded = str2num(input('How many pounds have you added?\n', 's'));
+    if i == 1
+        p(i) = weightadded;
+    else
+        p(i) = p(i-1)+weightadded;
+    end
+
+    start(ai)
+    wait(ai, duration+1)
+    data = getdata(ai);
+    v(i) = mean(data);
+    stop(ai)
+end
+
+coef = polyfit(v, p, 1)
+
+plot(v, p, '.', v, coef(1).*v+coef(2))
+
+delete(ai)
+clear ai chan
--- DAQSoftware.m
+++ DAQSoftware.m
@@ -5,11 +5,6 @@ end
 % clear up everything else
 close all; clear all; clc;
 
-% load the VectorNav library
-addpath('C:\Documents and Settings\Administrator\My Documents\MATLAB\VectorNavLib')
-% serial object for the VectorNav
-s = VNserial('COM3');
-
 % connect to the daq box
 ai = analoginput('nidaq','Dev1');
 ao = analogoutput('nidaq','Dev1');
@@ -19,8 +14,10 @@ ActualRate = get(ai,'SampleRate');
 set(ai,'SamplesPerTrigger',duration*ActualRate);
 set(ai,'TriggerType','Manual');
 set(ai,'InputType','SingleEnded');
-chan = addchannel(ai,[0 1 2 3 4]);
+chan = addchannel(ai,[21]);
+get(ai)
 ochan = addchannel(ao,0);
+
 five = [10 10]';
 putdata(ao,five)
 start(ao)
@@ -33,24 +30,20 @@ data = zeros(50,5);
 % make the figure
 figure(1)
 lines = plot(data); % plot the raw data
-%ylim([2.4 2.6])
+ylim([-13 13])
 ylabel('Voltage')
-leg = legend('Steer Angle', 'Steer Rate', 'Steer Torque', 'Wheel Speed', 'Button');
-labels = {'', '', '', '', ''};
+leg = legend('Pull Load Cell');
+labels = {''};
 
 % update the plot while the data is being taken
 while (1)
     % return the latest 50 samples
     data = peekdata(ai,100);
     for i = 1:5
-        set(lines(i), 'YData', data(:, i))
+        set(lines, 'YData', data)
     end
     meanData = mean(data);
     labels{1} = sprintf('Steer Angle = %1.2f V', meanData(1));
-    labels{2} = sprintf('Steer Rate = %1.2f V', meanData(2));
-    labels{3} = sprintf('Steer Torque = %1.2f V', meanData(3));
-    labels{4} = sprintf('Wheel Speed = %1.2f V', meanData(4));
-    labels{5} = sprintf('Button = %1.2f V', meanData(5));
     set(leg, 'String', labels)
     figure(1)
 end
