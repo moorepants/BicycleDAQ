464140aefd19c7d500e3919809095a5621a90bb5
Jason Moore <moorepants@gmail.com>
2011-02-11 17:53:42 -0800

    Steer angle calibration

--- tools/Anglecalibration.m
+++ tools/Anglecalibration.m
@@ -12,33 +12,42 @@ set(ai,'SampleRate',200)
 ActualRate = get(ai,'SampleRate');
 set(ai,'SamplesPerTrigger',duration*ActualRate);
 set(ai,'InputType','SingleEnded');
-chan = addchannel(ai,[21]);
+chan = addchannel(ai,[1,23]);
+%Vin=addchannel(ai,[23]);  %recording the input voltage
 
 % how many trials will you do?
-numTrials = str2num(input('How many trials?\n', 's'));
+numTrials = str2num(input('How many angles do you want to collect data at?\n', 's'));
 % initialize pull force and voltage vectors
 p = zeros(numTrials, 1);
 v = zeros(numTrials, 1);
+Vin=zeros(numTrials, 1);
 
 for i = 1:length(p)
     % ask the user to enter the number of pounds added
-    weightadded = str2num(input('How many pounds have you added?\n', 's'));
-    if i == 1
-        p(i) = weightadded;
-    else
-        p(i) = p(i-1)+weightadded;
-    end
-
+%     weightadded = str2num(input('How many pounds have you added?\n', 's'));
+%     if i == 1
+%         p(i) = weightadded;
+%     else
+%         p(i) = p(i-1)+weightadded;
+%     end
+    angle1=str2num(input('what is the angle reading on the protractor? \n', 's') );
+    %Angle beyond 50 degrees on each side overshoots the potentiometer. 
+    %calibrate for angles from -50 to +50, which is 21 data points
+    p(i)=angle1;
+    
     start(ai)
     wait(ai, duration+1)
     data = getdata(ai);
-    v(i) = mean(data);
+    v(i) = mean(data(:,1));
+    Vin(i)=mean(data(:,2));
     stop(ai)
 end
+
+
 
 coef = polyfit(v, p, 1)
 
 plot(v, p, '.', v, coef(1).*v+coef(2))
-
+grid on
 delete(ai)
 clear ai chan
