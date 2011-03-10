780e8b60aba0ea42ce43c2ddd51fbc99aa04acee
Jason Moore <moorepants@gmail.com>
2011-03-09 19:56:40 -0800

    renamed the fill_h5 file

--- tools/fill_h5.m
+++ tools/fill_h5.m
@@ -1,3 +1,12 @@
+function fill_h5(directory)
+
+dirinfo = what(directory);
+matfiles = dirinfo.mat;
+for i=1:length(matfiles)
+    filename = matfiles{i};
+    hdf5_save_test(filename(1:end-4));
+end
+
 function hdf5_save_test(runid)
 addpath('hdf5matlab')
 
@@ -12,12 +21,3 @@ hdf5save([todata filesep 'h5' filesep runid '.h5'], ...
          'VNavData', 'VNavData', ...
          'VNavDataText', 'VNavDataText', ...
          'par', 'par')
-
-function fill_h5(directory)
-
-dirinfo = what(directory);
-matfiles = dirinfo.mat;
-for i=1:length(matfiles)
-    filename = matfiles{i};
-    hdf5_save_test(filename(1:end-4));
-end
