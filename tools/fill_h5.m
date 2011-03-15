36322a0c97deccfdd804ae91a2feaecb27157c3d
Jason Moore <moorepants@gmail.com>
2011-03-15 11:18:23 -0700

    split up the m file so it is more useful

--- tools/fill_h5.m
+++ tools/fill_h5.m
@@ -6,19 +6,5 @@ for i=1:length(matfiles)
     filename = matfiles{i};
     display(sprintf('Saving: %s', matfiles{i}))
     hdf5_save_test(filename(1:end-4));
+    clear InputPairs NIData VNavCols VNavData VNavDataText par
 end
-
-function hdf5_save_test(runid)
-addpath('hdf5matlab')
-
-todata = ['..' filesep 'data'];
-
-load([todata filesep runid '.mat'])
-
-hdf5save([todata filesep 'h5' filesep runid '.h5'], ...
-         'InputPairs', 'InputPairs', ...
-         'NIData', 'NIData', ...
-         'VNavCols', 'VNavCols', ...
-         'VNavData', 'VNavData', ...
-         'VNavDataText', 'VNavDataText', ...
-         'par', 'par')
