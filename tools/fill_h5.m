f1a5d1650c0a07b25d6e9ddb454710ba3b42e58f
Jason Moore <moorepants@gmail.com>
2011-03-16 17:26:18 -0700

    upgraded the fill_h5 function so it is a little smarter and does not save if it doesn't need to

--- tools/fill_h5.m
+++ tools/fill_h5.m
@@ -1,40 +1,29 @@
-function fill_h5(directory)
+function fill_h5(directory, all)
+%Parameters
+%----------
+%directory : string
+%   ../data
+%all : 0 or 1
+%   0 if you only want the lastest mat files to be converted, and 1 if you
+%   want all the mat files to be converted.
+
 % find out what files are in h5 directory
 h5info = dir([directory filesep 'h5']);
-h5names = cell(length(h5info), 1);
-lasth5 = str2num(h5info(end).names)
-bad = [];
-j = 1;
-for i = 1:length(h5info)
-    if strcmp(h5info(i).name, '.') || strcmp(h5info(i).name, '..')
-        bad(j) = i;
-        j = j + 1;
-    else
-        h5names{i} = h5info(i).name(1:end-3);
-    end
-end
-% remove the . and ..
-h5names(bad) = [];
-% sort
-h5names = sort(h5names);
+lasth5 = str2num(h5info(end).name(1:end-3));
 
 dirinfo = what(directory);
 matfiles = dirinfo.mat;
-lastmat = str2num(matfiles{end})
-matnames = cell(length(matfiles), 1);
-for i = 1:length(matnames)
-    fname = matfiles{i};
-    matnames{i} = fname(1:end-4);
-end
-matnames = sort(matnames);
+lastmat = matfiles{end};
+lastmat = str2num(lastmat(1:end-4));
 
 if all == 0
-    matfiles = matfiles{lasth5:lastmat)
+    % the +2 is because of the . and .. in h5info.names
+    matfiles = matfiles(lasth5+2:lastmat);
 end
     
-% for i=1:length(matfiles)
-%     filename = matfiles{i};
-%     display(sprintf('Saving: %s', matfiles{i}))
-%     hdf5_save_test(filename(1:end-4));
-%     clear InputPairs NIData VNavCols VNavData VNavDataText par
-% end
+for i=1:length(matfiles)
+    filename = matfiles{i};
+    display(sprintf('Saving: %s', matfiles{i}))
+    hdf5_save(filename(1:end-4));
+    clear InputPairs NIData VNavCols VNavData VNavDataText par
+end
